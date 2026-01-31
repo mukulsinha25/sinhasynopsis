bdd415d4a1a4c204ccea3d933fb558502ea87a68
mukulsinha25 <90440641+mukulsinha25@users.noreply.github.com>
2026-01-31 14:16:54 +0530

    Update About.js

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,6 +1,7 @@
 import "../App.css";
 import profilePic from "../assets/mukul.jpeg";
 import bgVideo from "../assets/background.mp4";
+import { Link } from "react-router-dom";
 
 function About() {
   return (
@@ -16,9 +17,13 @@ function About() {
       <header className="header">
         <div className="logo">Sinha Synopsis<span>.</span></div>
 
-        <nav className="nav">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
+        // <nav className="nav">
+        //   <a href="/">Home</a>
+        //   <a href="/about">About</a>
+        // </nav>
+      <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
         </nav>
       </header>
 
